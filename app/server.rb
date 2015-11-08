15e1f8e6bc8fc1176531bd35dbcc733730125df6
latticetower <merlettaia@gmail.com>
2015-11-08 03:28:43 +0300

    added index page and repo page

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,84 @@
 require 'sinatra'
+require 'slim'
+require 'octokit'
 
-get '/' do
-  "Hello World!"
+require File.dirname(__FILE__) + '/repo.rb'
+require File.dirname(__FILE__) + '/redis_store.rb'
+
+# 6224b76420f3d4f36d7152f751cbeac2fff76891
+OCTOKIT_CLIENT_ID = ENV['OCTOKIT_CLIENT_ID']
+OCTOKIT_CLIENT_SECRET = ENV['OCTOKIT_CLIENT_SECRET']
+
+use Rack::Session::Cookie, :secret => rand.to_s()
+
+def authenticated?
+  session[:access_token]
+end
+
+def authenticate!
+  client = Octokit::Client.new 
+  url = client.authorize_url OCTOKIT_CLIENT_ID, 
+  	:scope => 'user:email'
+
+  redirect url
+end
+
+get '/' do 
+	@recent_repos = RedisStore.new().get_recent_repos()
+	slim :index
+end
+
+get '/repo/:owner/:name' do
+	owner = params['owner']
+	name = params['name']
+
+	@repo = RedisStore.new().get_repo(owner + '/' + name)
+
+	slim :repo
+end
+
+post '/repo' do
+	full_name = params['query']
+	parts = full_name.split("/")
+	owner = parts[0]
+	name = parts[1]
+	#todo: check in cache
+	#todo: chech on github
+
+	client = Octokit::Client.new \
+		:client_id => OCTOKIT_CLIENT_ID, 
+		:client_secret => OCTOKIT_CLIENT_SECRET
+	exists = client.repository? :repo => name, :owner => owner
+	
+	if !exists then
+		redirect to('/')
+	else		
+		result = client.repo :repo => name, :owner => owner
+		branch = client.ref full_name, "heads/master"
+	
+
+		repo = Repo.new \
+			:id => result.id,
+			:owner => result.owner.login,
+			:name => result.name,
+			:url => result.html_url,
+			:last_commit => branch.object.sha,
+			:update_ts => Time.now.to_i
+
+		RedisStore.new().put_repo(repo)
+
+		redirect to('/repo/' + owner + '/' + name)
+	end
+end
+
+get '/login' do
+	authenticate!
+end
+
+get '/login_callback' do
+  session_code = request.env['rack.request.query_hash']['code']
+  result = Octokit.exchange_code_for_token(session_code, CLIENT_ID, CLIENT_SECRET)
+  session[:access_token] = result[:access_token]
+
+  redirect '/'
 end
