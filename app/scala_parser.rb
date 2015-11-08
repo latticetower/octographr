3f27dbda7c6f207a61ea71dda49889935bddf89d
Tatiana Malygina <merlettaia@gmail.com>
2015-11-08 08:56:53 +0300

    added more rules

--- app/scala_parser.rb
+++ app/scala_parser.rb
@@ -8,24 +8,44 @@ class ScalaParser < Parslet::Parser
   rule(:word_import) { str('import') }
   rule(:word_val) { str('val') }
   rule(:word_var) { str('var') }
+  rule(:word_extends) { str('extends') }
+  rule(:word_with) { str('with') }
   rule(:colon) { str(':') }
-
-  rule(:class_name) { match('[a-zA-Z]').repeat(1) }
 
   rule(:space)      { match('\s').repeat(1) }
   rule(:space?)     { space.maybe }
 
+  rule(:class_name) { match('[a-zA-Z]').repeat(1) }
+  rule(:parent_classes) { space? >> word_extends >> space >>
+    class_name.as(:parent_type) >> (space? >> word_with >> space >> class_name.as(:parent_type)).repeat >> space? }
+
+
   rule(:lparen) { str('(') }
   rule(:rparen) { str(')') }
+  rule(:not_rparen) { match('[^)]*') }
+  rule(:lbracket) {str('{')}
+  rule(:rbracket) {str('}')}
+  rule(:not_rbracket) { match('[^}]*') }
+  rule(:comma) { space? >> str(',') >> space? }
+
+
+  #todo:add rules to ignore comments
 
   rule(:var_name) { match('[a-zA-Z0-9_]').repeat(1) }
   rule(:var_type) { match('[a-zA-Z0-9_]').repeat(1) }
   rule(:var_expr) { space? >> (word_val | word_val).maybe >> space? >> var_name >>
       space? >> (colon >> space? >> var_type.as(:var_type)).maybe }
+  rule(:var_expressions) { var_expr >> (comma >> var_expr).repeat}
+
+  rule(:package_name) { match('a-zA-Z_-').repeat(1) }
+  #todo: add complex rule to ignored {} contents
 
 
+  rule(:package_expr) { word_package >> space >> package_name >> (str('.') >> package_name).repeat.maybe >> endl}
+  rule(:import_expr) { word_import >> space >> package_name >> (str('.') >> package_name).repeat.maybe >> endl}
+  rule(:trait_expr) { word_trait >> space >> class_name >> space? }
   rule(:class_expr) { space? >> (word_case >> space).maybe >> word_class >>
-    space >> class_name.as(:current_class) >> space? >> (lparen >> var_expr.maybe >> rparen).maybe}
-  rule(:some_expr) { word_trait | class_expr }
+    space >> class_name.as(:current_class) >> space? >> (lparen >> var_expressions.maybe >> rparen).maybe >> parent_classes.maybe}
+  rule(:some_expr) { package_expr.maybe | (import_expr | trait_expr | class_expr).repeat }
   root(:some_expr)
 end
--- spec/scala_parser_spec.rb
+++ spec/scala_parser_spec.rb
@@ -6,14 +6,17 @@ require __dir__ + '/../app/scala_parser.rb'
 describe ScalaParser, "primitive test" do
   let(:parser) { ScalaParser.new }
 
-  describe "new method call" do
+  describe "ScalaParser" do
     it "should parse simple word constants" do
       expect(parser.word_class).to parse('class')
       expect(parser.word_trait).to parse('trait')
       expect(parser.word_case).to parse('case')
+      expect(parser.word_package).to parse('package')
     end
     it "should parse simple var names and class names" do
       expect(parser.class_name).to parse("Rrrrrr")
+    end
+    it "should parse string with class name in different situations" do
     end
   end
 end
